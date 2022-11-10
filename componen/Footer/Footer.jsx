be37ee4f5659197ca00f48ba1698f8ae06637d5d
abdoachhoubi <aachhoub@gmail.com>
2022-11-10 21:39:52 +0100

    Fixing Bugs

---
+++ componen/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <footer>
+      <Link href="https://aachhoub.vercel.app" target="_blank" rel="noreferrer">
+        <h4>Astroboy &copy; All rights reserved</h4>
+      </Link>
+    </footer>
+  );
+};
+
+export default Footer;
